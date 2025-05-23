87a85513ed392961fd4d97b41ca6e1467ae3bdab
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-05-23 17:41:25 -0300

    Appended/ directorie for unknow things in the doc section

--- src/app.rb
+++ src/app.rb
@@ -2,9 +2,6 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sqlite3'
 
-set :database, {adapter: "sqlite3", database: "db/wallet_development.sqlite3"}
-
-
 require_relative 'frontend'
 require_relative 'backend/backendEndpoint'
 require_relative 'backend/model/user'
@@ -14,16 +11,4 @@ require_relative 'backend/model/movement'
 require_relative 'backend/model/transfer'
 
 
-require 'sinatra'
-
-get '/' do
-  erb :login
-end
-
-get '/main' do
-  erb :main
-end
-
-get '/transfer' do
-  erb :transfer
-end
+set :database, {adapter: "sqlite3", database: "db/wallet_development.sqlite3"}

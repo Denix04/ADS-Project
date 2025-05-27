13cfccf972814d7ade05cc5114f0b2a40b904176
camiastrada <camiastradaw@gmail.com>
2025-05-27 17:52:34 -0300

    Modified / verificaciones account y verificaciones users

--- src/backend/model/account.rb
+++ src/backend/model/account.rb
@@ -1,7 +1,11 @@
 class Account < ActiveRecord::Base
-  has_many :sent_transactions, class_name: 'Transaction', foreign_key: 'origin_id'
-  has_many :received_transactions, class_name: 'Transaction', foreign_key: 'destination_id'
-
+  belongs_to :user
   has_many :sent_movements, class_name: 'Movement', foreign_key: 'origin_id'
   has_many :received_movements, class_name: 'Movement', foreign_key: 'destination_id'
+
+  validates :cbu, uniqueness: true, allow_nil: true
+  validates :alias, uniqueness: true, allow_nil: true
+  validates :numcuenta, uniqueness: true, allow_nil: true
+  validates :saldo, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- src/backend/model/user.rb
+++ src/backend/model/user.rb
@@ -1,4 +1,8 @@
 require 'active_record'
-
 class User < ActiveRecord::Base
+    validates :dni, presence: true, uniqueness: true
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password_digest, presence: true
+
 end

1cff96a01c9ebac0b8d1826ce6b64998db04ce86
camiastrada <camiastradaw@gmail.com>
2025-06-11 17:57:47 -0300

    Refactored / migraciones

--- db/migrate/20250521011914_add_dni_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddDniToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :last_name, :string
-    add_column :users, :dni, :string
-    add_column :users, :localidad, :string
-  end
-end
--- db/migrate/20250527205932_add_service_account.rb
+++
@@ -1,5 +0,0 @@
-class AddServiceAccount < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :movements, :service_account, foreign_key: true, null: true
-  end
-end

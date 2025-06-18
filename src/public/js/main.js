01c9ee617aa9e09684e9140849e327d47b89cdcf
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-06-18 17:52:11 -0300

    Fix: back button in perfil

--- src/public/js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("transfer_btn").addEventListener("click", () => {
-    window.location.href = "/transfer"
-  });
-});
